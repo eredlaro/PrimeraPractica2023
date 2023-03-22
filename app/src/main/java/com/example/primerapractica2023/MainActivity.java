26105842421dbde53350234c2f8ee7698afc07bf
eredlaro <eredlaro@gmail.com>
2023-03-22 09:46:51 -0600

    agregue shared preferences

--- app/src/main/java/com/example/primerapractica2023/MainActivity.java
+++ app/src/main/java/com/example/primerapractica2023/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.primerapractica2023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,12 +16,16 @@ public class MainActivity extends AppCompatActivity {
 
     private Button next;
     private EditText captura;
+    private SharedPreferences preferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         next = (Button) findViewById(R.id.next);
         captura = (EditText)findViewById(R.id.captura);
+        preferences = getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+
+
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,10 +33,17 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"Hiciste Click",Toast.LENGTH_SHORT).show();
                 Log.i("INFO:","Pasaste a la segunda pantalla");
                 String cadena = captura.getText().toString();
+                SaveCred(cadena);
                 Intent intent = new Intent(MainActivity.this,MainActivity2.class);
                 intent.putExtra("informacion",cadena);
                 startActivity(intent);
             }
         });
+    }
+
+    private void SaveCred(String cadena) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("informacion", cadena);
+        editor.commit();
     }
 }
--- app/src/main/java/com/example/primerapractica2023/MainActivity2.java
+++ app/src/main/java/com/example/primerapractica2023/MainActivity2.java
@@ -2,7 +2,9 @@ package com.example.primerapractica2023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,6 +15,7 @@ import android.widget.Toast;
 public class MainActivity2 extends AppCompatActivity {
     private Button back;
     private TextView mensaje;
+    private SharedPreferences preferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +24,9 @@ public class MainActivity2 extends AppCompatActivity {
         back = (Button) findViewById(R.id.back);
         mensaje = (TextView) findViewById(R.id.mensaje);
         GetMensaje();
-
+        preferences = getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+        String informacion = preferences.getString("informacion","");
+        Toast.makeText(getApplicationContext(),informacion,Toast.LENGTH_SHORT).show();
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
